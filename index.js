6574b599555ca2b5f174c1a4325d23cba10ec8b5
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-04-15 17:00:20 +0530

    properties of js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-alert("hello");
-document.querySelector("h1").innerHTML = "good bye";
+document.querySelector("h1").innerHTML = "<em>hello world</em>";
+document.querySelector("h1").innerText;
+document.querySelector("h1").innerHTML;
+document.querySelector("a").attributes;
