f3cb1e38dabcb1336ebbf1ca098a2d08bbafee5e
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-05-27 02:03:52 +0530

    Update script.js

--- challenges/challenge1/script.js
+++ challenges/challenge1/script.js
@@ -5,8 +5,12 @@ function ageInDays() {
     var ageInDays = (2020 - birthYear) * 365;
     //console.log(ageInDays);
     var h1 = document.createElement('h1');
-    var textAnswer = document.createTextNode('You are' + ageInDays+'days old.');
+    var textAnswer = document.createTextNode('You are' + ageInDays + 'days old.');
     h1.setAttribute('id', 'ageInDays');
     h1.appendChild(textAnswer);
     document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
 }
