a25ee06528fc808a51f82f28a5d4488382d4fe8f
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-05-28 01:28:21 +0530

    Add files via upload

---
+++ challenges/challenge2/script.js
@@ -0,0 +1,23 @@
+//alert('hello');
+//CHALLENGE 1: YOUR AGE IN DAYS
+function ageInDays() {
+    var birthYear = prompt('what year were you born ..good friend');
+    var ageInDays = (2020 - birthYear) * 365;
+    //console.log(ageInDays);
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are' + ageInDays + 'days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://i.imgur.com/krb2nFS.gif";
+    div.appendChild(image);
+}
